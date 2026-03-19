92b9be4fd48f82b24a251269153d245e55cfeb33
Lan3 <lanarikaiser@gmail.com>
2026-03-19 19:11:54 +0100

    ejemploPOO

---
+++ ejerciciosPython2/ejemploPOO.py
@@ -0,0 +1,9 @@
+class Persona:
+    def __init__(self, nombre, edad): 
+        self.nombre = nombre 
+        self.edad = edad
+
+    def saludar(self):
+        print(f"Hola, mi nombre es {self.nombre} y tengo {self.edad} años.")
+persona1 = Persona("Ángel", 20)
+persona1.saludar()
